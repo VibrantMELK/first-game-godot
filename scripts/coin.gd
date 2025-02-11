e13bab4ba04f59a59f9e4e9fb26932bfed01b64d
VibrantMELK <gatchaemilie@gmail.com>
2025-02-11 10:02:57 +0100

    legget til lyd når spilleren plukker opp kroner

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,11 +1,12 @@
 extends Area2D
 
 @onready var game_manager: Node = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(body):
 	game_manager.add_point()
-	queue_free()
+	animation_player.play("pickup")
 	
