a2631b5ba16268da745964cc6d9d99ae1984489d
VibrantMELK <gatchaemilie@gmail.com>
2025-02-12 14:18:36 +0100

    tokk bort single player med kode

--- scripts/multiplayer/multiplayer_manager.gd
+++ scripts/multiplayer/multiplayer_manager.gd
@@ -3,8 +3,14 @@ extends Node
 const SERVER_PORT = 8080
 const SERVER_IP = "127.0.0.1"
 
+var multiplayer_scene = preload("res://scenes/multiplayer_player.tscn")
+
+var _player_spawn_node
+
 func become_host():
 	print("Starting host!")
+	
+	_player_spawn_node = get_tree().get_current_scene().get_node("Players")
 	
 	var server_peer = ENetMultiplayerPeer.new()
 	server_peer.create_server(SERVER_PORT)
@@ -13,6 +19,8 @@ func become_host():
 	
 	multiplayer.peer_connected.connect(_add_player_to_game)
 	multiplayer.peer_disconnected.connect(_del_player)
+	
+	_remove_single_player()
 
 func join_as_player_2():
 	print("Player 2 joining")
@@ -21,9 +29,22 @@ func join_as_player_2():
 	client_peer.create_client(SERVER_IP, SERVER_PORT)
 	
 	multiplayer.multiplayer_peer = client_peer
+	
+	_remove_single_player()
 
 func _add_player_to_game(id: int):
 	print("Player % joined the game!" % id)
+	
+	var player_to_add = multiplayer_scene.instantiate()
+	player_to_add.player_id = id
+	player_to_add.name = str(id)
+
+	_player_spawn_node.add_child(player_to_add, true)
 
 func _del_player(id: int):
 	print("Player % left the game!" % id)
+	
+func _remove_single_player():
+	print("Remove single player")
+	var player_to_remove = get_tree().get_current_scene().get_node("Player")
+	player_to_remove.queue_free()
