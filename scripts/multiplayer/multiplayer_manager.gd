4f17453500af4cd0d915835b2066326884d99402
VibrantMELK <gatchaemilie@gmail.com>
2025-02-12 09:27:27 +0100

    prøver å legge til server

--- scripts/multiplayer/multiplayer_manager.gd
+++ scripts/multiplayer/multiplayer_manager.gd
@@ -1,7 +1,23 @@
 extends Node
 
+const SERVER_PORT = 8080
+const SERVER_IP = "127.0.0.1"
+
 func become_host():
 	print("Starting host!")
+	
+	var server_peer = ENetMultiplayerPeer.new()
+	server_peer.create_server(SERVER_PORT)
+	
+	mutliplayer.multiplayer_peer = server_peer
+	
+	multiplayer.peer_connected.connect(_add_player_to_game)
+	multiplayer.peer_disconnected.connect(_del_player)
 
 func join_as_player_2():
 	print("Player 2 joining")
+	
+
+
+
+func _add_player_to_game(id: int):
